77a8337285a43ee232dd4932e9519d204ee86c00
Adetule_ayomide <adetuleayomide@gmail.com>
2023-02-16 13:07:55 -0800

    C program that print programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - a C program that prints exactly "Programming is like building a multilingual puzzle
  * return: 0 (success)
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
