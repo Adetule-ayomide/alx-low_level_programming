c74970f0ebc3f7d7b5287ed7ca40a9655a23a049
Adetule_ayomide <adetuleayomide@gmail.com>
2023-02-16 14:10:50 -0800

    programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - a C program that prints exactly "Programming is like building a multilingual puzzle
+ * main - a C program that prints exactly some lines of words
  * return: 0 (success)
 */
 
