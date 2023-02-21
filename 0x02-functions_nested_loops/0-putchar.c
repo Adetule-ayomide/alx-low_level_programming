c49c22ed9b319fc31b13a8ba76096fd7e5ab6ca2
Adetule_ayomide <adetuleayomide@gmail.com>
2023-02-21 07:18:04 -0800

    a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#incude "main.h"
+
+/**
+ * main - prints _putchar and end it with a new line
+ *
+ * return: 1 on success
+ */
+
+int main(void)
+{	
+	int _putchar(char c);
+
+	_putchar("_")
+	_putchar("p")
+	_putchar("u")
+	_putchar("t")
+	_putchar("c")
+	_putchar("h")
+	_putchar("a")
+	_putchar("r")
+	_purchar("\n")
+
+	return (_purchar(1, &c, 1)
+}
