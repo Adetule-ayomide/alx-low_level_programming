1dca47610d63a2a57d7b0fc32b70166040871118
Adetule_ayomide <adetuleayomide@gmail.com>
2023-02-21 11:26:36 -0800

    a function that checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lowercase character
  * @c: is a char to be checked
- * Returns: 1 and 0 depending on the condition
+ * Return: 1 and 0 depending on the condition
  */
 
 int _islower(int c)
