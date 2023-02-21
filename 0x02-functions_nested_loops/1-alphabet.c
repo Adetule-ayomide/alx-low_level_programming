80bc2402b63149a7c8f55861441e59b36085a28d
Adetule_ayomide <adetuleayomide@gmail.com>
2023-02-21 09:07:21 -0800

    a function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -  prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+
+
+}	
