79abfacc6cf4707f538f6ba5faad68eedf3231dd
Adetule_ayomide <adetuleayomide@gmail.com>
2023-02-24 16:19:30 -0800

    a function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,20 +8,27 @@
 
 void print_most_numbers(void)
 {
-	int c;
 
-	for (c = 48; c < 58; c++)
+int c;
 
-	{
-		if (c != 50)
+for (c = 48; c < 58; c++)
 
-	if (c != 52)
-	{
-		_putchar(c);
-	}
+{
 
-	}
-	_putchar('\n');
+if (c != 50)
+
+{
+
+if (c != 52)
+
+{
+
+_putchar(c);
 
 }
 
+}
+
+}
+_putchar('\n');
+}
