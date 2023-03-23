d24c02a3eddc32a342125c8ef283d912c98efbc7
Adetule-ayomide <adetuleayomide@gmail.com>
2023-03-22 20:31:30 -0700

    a function in c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,21 +1,16 @@
 #include "function_pointers.h"
 /**
- * array_iterator - this functions iterates an array
+ * print_name- this functions prints a name
 (* a blank line
- *@array: the array to iterate
- *@size: the size of the array.
- *@action: this a function pointer.
-* Description: this function iterate an array)?
+ *@name: string to print.
+ *@f: this a function pointer, that recieves a string.
+* Description: this function prints a name)?
 (* section header: the header of this function is function_pointers.h)*
 * Return: this is a void function no return
 */
-void array_iterator(int *array, size_t size, void (*action)(int))
+void print_name(char *name, void (*f)(char *))
 {
-	size_t i;
-
-	if (array && size && action)
-	{
-	for (i = 0; i < size; i++)
-		action(*(array + i));
-	}
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
 }
