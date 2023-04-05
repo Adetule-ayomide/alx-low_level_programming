d151c2c5216ee1111b92bdec7c87a7d84ba87b95
Adetule-ayomide <adetuleayomide@gmail.com>
2023-04-04 17:35:29 -0700

    function that returns the number of elements in a linked listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,9 @@
 #include "lists.h"
-
 /**
 * print_listint - struct node variable in the prototype
 * @h: A pointer to the next node
 * Return: numNode
 */
-
 size_t print_listint(const listint_t *h)
 {
 	int numNode = 0;
