62086269f82faefea5393bd4758ed642f5fd4620
Adetule-ayomide <adetuleayomide@gmail.com>
2023-04-10 19:23:58 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,27 +6,29 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned integer = 0;
-int length, binary;
+	unsigned int integer;
+	int length, binary;
 
-if (!b)
-return (0);
+	integer = 0;
 
-for (length = 0; b[length] != '\0'; len++)
-;
+	if (!b)
+	return (0);
 
-for (length--, binary = 1; length >= 0; length--, binary *= 2)
-{
-if (b[length] != '0' && b[length] != '1')
-{
-return (0);
-}
+	for (length = 0; b[length] != '\0'; len++)
+	;
 
-if (b[length] & 1)
-{
-integer += binary;
-}
-}
+	for (length--, binary = 1; length >= 0; length--, binary *= 2)
+	{
+		if (b[length] != '0' && b[length] != '1')
+		{
+			return (0);
+		}
+
+		if (b[length] & 1)
+		{
+			integer += binary;
+		}
+	}
 
 return (integer);
 }
