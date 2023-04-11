4ce34d0118e110e40ea72eed5cab3efe91363b3e
Adetule-ayomide <adetuleayomide@gmail.com>
2023-04-10 19:08:32 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,33 @@
 #include "main.h"
 /**
- * binary_to_uint - The prototype which convert binary to unsigned integer
- * @b: A pointer
- * Return: Integer if success or 0 if failed
- */
+* binary_to_uint - The prototype which convert binary to unsigned integer
+* @b: A pointer
+* Return: Integer if success or 0 if failed
+*/
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned integer = 0;
-        int length, binary;
+unsigned integer = 0;
+int length, binary;
 
-        if (!b)
-                return (0);
+if (!b)
+return (0);
 
-        for (length = 0; b[length] != '\0'; len++)
-                ;
+for (length = 0; b[length] != '\0'; len++)
+;
 
-        for (length--, binary = 1; length >= 0; length--, binary *= 2)
-        {
-                if (b[length] != '0' && b[length] != '1')
-                {
-                        return (0);
-                }
+for (length--, binary = 1; length >= 0; length--, binary *= 2)
+{
+if (b[length] != '0' && b[length] != '1')
+{
+return (0);
+}
 
-                if (b[length] & 1)
-                {
-			integer += binary;
-                }
-        }
+if (b[length] & 1)
+{
+integer += binary;
+}
+}
 
-        return (integer);
+return (integer);
 }
 
