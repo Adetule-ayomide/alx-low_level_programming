138f85f5cc208651ab1fd0dbeb12682afa84aa2a
Adetule-ayomide <adetuleayomide@gmail.com>
2023-04-10 18:06:08 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC| O_APPEND, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_checks(file_to, file_from, argv);
 	read_f = 1024;
 	while (read_f == 1024)
