b4fcc4984e0ea327cfe1d9f38f32ac56c395d6a0
Adetule-ayomide <adetuleayomide@gmail.com>
2023-05-04 16:17:39 -0700

    a function that returns value

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index
+ * @n: integer
+ * @index: position of the integer to be move
+ * Return: -1 if failed and cal if success
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int cal;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	cal = n >> index;
+
+	cal &= 1;
+	return (cal);
+}
