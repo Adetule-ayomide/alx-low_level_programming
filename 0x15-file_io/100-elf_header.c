e2c18a5c1d84911b3afe473e722f86de0a5e95d2
Adetule-ayomide <adetule>
2023-05-09 15:54:11 -0700

    file input/output

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -23,8 +23,11 @@ void close_elf(int elf);
 
 void print_magic(unsigned char *e_ident)
 {
+	int i;
+	
 	printf("Magic:   ");
-	for (int i = 0; i < EI_NIDENT; i++)
+	
+	for (i = 0; i < EI_NIDENT; i++)
 	{
 		printf("%02x ", e_ident[i]);
 	}
@@ -64,7 +67,7 @@ void print_class(unsigned char *e_ident)
 void print_data(unsigned char *e_ident)
 {
 	printf("Data:                              ");
-	switch (header.e_ident[EI_DATA])
+	switch (e_ident[EI_DATA])
 	{
 	case ELFDATA2LSB:
 		printf("2's complement, little endian\n");
@@ -160,6 +163,9 @@ void print_abi(unsigned char *e_ident)
 
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+		e_type >>= 8;
+
 	printf("Type:                              ");
 	switch (e_type)
 	{
@@ -208,6 +214,46 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 }
 
 /**
+ * close_elf - Closes an ELF file.
+ * @elf: The file descriptor of the ELF file.
+ *
+ * Description: If the file cannot be closed - exit code 98.
+ */
+void close_elf(int elf)
+{
+	if (close(elf) == -1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't close fd %d\n", elf);
+		exit(98);
+	}
+}
+
+/**
+ * check_elf - Checks if a file is an ELF file.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
+ * Description: If the file is not an ELF file - exit code 98.
+ */
+
+void check_elf(unsigned char *e_ident)
+{
+	int check_it;
+
+	for (check_it = 0; check_it < 4; check_it++)
+	{
+		if (e_ident[check_it] != 127 &&
+			e_ident[check_it] != 'E' &&
+			e_ident[check_it] != 'L' &&
+			e_ident[check_it] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
+
+/**
  * main - Displays the information contained in the
  *        ELF header at the start of an ELF file.
  * @argc: The number of arguments supplied to the program.
@@ -219,12 +265,12 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
  *              the function fails - exit code 98.
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
 	int pd, bd;
 
-	int pd = open(argv[1], O_RDONLY);
+	pd = open(argv[1], O_RDONLY);
 
 	if (pd == -1)
 	{
@@ -239,7 +285,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	bd = read(pd, header, sizeof(Elf64_Ehdr));
-	if (b == -1)
+	if (bd == -1)
 	{
 		free(header);
 		close_elf(pd);
