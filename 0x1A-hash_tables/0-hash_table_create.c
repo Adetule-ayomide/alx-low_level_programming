536ac0815973522ebcf904b680691405fbe04126
Adetule-ayomide <adetuleayomide@gmail.com>
2023-09-10 14:33:52 -0700

    a function that creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,12 +15,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
-	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
-
+	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
@@ -28,6 +27,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	new_table->size = size;
 	return (new_table);
 }
