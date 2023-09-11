b681f554a1bc3339130a1e227353808db8ae164c
Adetule-ayomide <adetuleayomide@gmail.com>
2023-09-10 18:46:58 -0700

    a function that retrieves a value associated with a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,8 +4,9 @@
  * hash_table_get - function that retrieves a value associated with a key.
  * @ht:  hash table you want to look into
  * @key: the key to the value to retrieve
- * @value: the value to retrieve
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ *
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
